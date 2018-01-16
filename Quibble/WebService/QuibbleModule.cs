8778818a36946119ad3ad9116dc16695ea1f22d1
Eduardo <eduardo.cxcorreio@gmail.com>
2018-01-16 02:17:38 +0100

    Created NancyModule and endpoints for GetQuibbles and GetQuibbleById with a route parameter.

---
+++ Quibble/WebService/QuibbleModule.cs
@@ -0,0 +1,30 @@
+﻿using Data;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class QuibbleModule : NancyModule
+    {
+        public QuibbleModule()
+        {
+            Get["/quibbles"] = arguments => GetQuibbles();
+            Get["/quibbles/{id}"] = arguments => GetQuibbleById(arguments.id);
+        }
+
+        private Quibble[] GetQuibbles()
+        {
+            var service = new QuibbleDataService();
+            return service.GetAll();
+        }
+
+        private Quibble GetQuibbleById(int id)
+        {
+            var service = new QuibbleDataService();
+            return service.GetById(id);
+        }
+    }
+}
